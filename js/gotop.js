e17c5cca7e602810f7d8655b1e8e2ff38b722656
Joseph David Ninaquispe Florez <54923488+jdavid2019@users.noreply.github.com>
2020-09-24 16:43:56 -0500

    Add files via upload

---
+++ js/gotop.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+$('.go-top-button').click(function(){
+ $('body,html').animate({
+  scrollTop:'0px',
+  behavior: 'smooth'
+ });
+});
+
+
+$(window).scroll(function(){
+	console.log(document.documentElement.scrollTop);
+	if(document.documentElement.scrollTop >120){
+	document.querySelector('.go-top-container').classList.add('show');	
+	}else{
+	document.querySelector('.go-top-container').classList.remove('show');	
+	}
+});
+
+});
+
